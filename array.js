5edaa1d25b9a4f50542255c295c2659c6cd6c23c
Ashraful Alam <ashrafcse3@gmail.com>
2022-08-09 00:18:28 +0100

    added array.js and simple-cart.js

---
+++ array.js
@@ -0,0 +1,15 @@
+const country = 'Bangladesh';
+const age = 52;
+const isIndependent = true;
+const student = { id: 132, class: 10, name: 'batash' };
+const friends = [13, 14, 21, 54, 82, 02];
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isIndependent);
+console.log(typeof student);
+console.log(Array.isArray(friends));
+console.log(typeof add);
---
+++ simple-cart.js
@@ -0,0 +1,9 @@
+// calculate the sum for all prices list
+const shoppingCart = [
+    { name: 'shirt', price: 1200 },
+    { name: 'pant', price: 2000 },
+    { name: 'Sika genji', price: 300 },
+    { name: 'Juta', price: 1800 }
+];
+
+console.log(shoppingCart);
