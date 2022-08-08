51c4ed3930a4247fa6ccf8cbd14bc2c3acf4c71c
Ashraful Alam <ashrafcse3@gmail.com>
2022-08-09 00:34:56 +0100

    finished simple-cart.js

--- simple-cart.js
+++ simple-cart.js
@@ -1,9 +1,19 @@
 // calculate the sum for all prices list
 const shoppingCart = [
-    { name: 'shirt', price: 1200 },
+    { name: 'shirt', price: 2200 },
     { name: 'pant', price: 2000 },
-    { name: 'Sika genji', price: 300 },
+    { name: 'Sika genji', price: 500 },
     { name: 'Juta', price: 1800 }
 ];
 
-console.log(shoppingCart);
+function getTotalCartPrice(cartList) {
+    let sum = 0;
+    for (let i = 0; i < cartList.length; i++) {
+        let element = cartList[i];
+        sum = sum + element.price;
+    }
+    return sum;
+}
+
+const totalCartPrice = getTotalCartPrice(shoppingCart);
+console.log(totalCartPrice);
